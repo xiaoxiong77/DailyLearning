d7d012e002af890ef8250d5d4075e6fa2f8e6517
xiaoxiong77 <17601397715@163.com>
2020-11-10 20:42:20 +0800

    nuxt demo

--- task-03-03-Vuex_SSR/code/Nuxt_realword/api/article.js
+++ task-03-03-Vuex_SSR/code/Nuxt_realword/api/article.js
@@ -26,10 +26,26 @@ export const addFavorite = slug => {
     })
   }
   
-  // 取消点赞
+// 取消点赞
 export const deleteFavorite = slug => {
     return request({
         method: 'DELETE',
         url: `/api/articles/${slug}/favorite`
     })
-  }
+}
+
+// 获取文章详情
+export const getArticle = slug => {
+    return request({
+        method: 'GET',
+        url: `/api/articles/${slug}`
+    })
+}
+
+// 获取文章评论
+export const getComments = slug => {
+    return request({
+        method: 'GET',
+        url: `/api/articles/${slug}/comments`
+    })
+}
